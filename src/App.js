f1070b9a93f993a5b426a5307373e15410d0523c
Mealea Hunter <h.mealea@gmail.com>
2020-04-11 15:44:27 -0400

    Add Search and Menu ability for onClick icon buttons

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import fetch from 'cross-fetch';
 import './App.scss';
 import 'typeface-roboto';
 import AppBar from '@material-ui/core/AppBar';
@@ -8,33 +9,152 @@ import MenuIcon from '@material-ui/icons/Menu';
 import SearchIcon from '@material-ui/icons/Search';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
+import Popover from '@material-ui/core/Popover';
+import Autocomplete from '@material-ui/lab/Autocomplete';
+import TextField from '@material-ui/core/TextField';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
 
-const useStyles = makeStyles(() => ({
+const useStyles = makeStyles((theme) => ({
   grow: {
     flexGrow: 1
-  }
+  },
+  typography: {
+    padding: theme.spacing(2),
+  },
 }));
+
+function sleep(delay = 0) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, delay);
+  });
+}
 
 function App() {
   const classes = useStyles();
+  const [anchorEl, setAnchorEl] = React.useState(null);
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const o = Boolean(anchorEl);
+  const id = o ? 'simple-popover' : undefined;
+
+  const [open, setOpen] = React.useState(false);
+  const [options, setOptions] = React.useState([]);
+  const loading = open && options.length === 0;
+
+  React.useEffect(() => {
+    let active = true;
+
+    if (!loading) {
+      return undefined;
+    }
+
+    (async () => {
+      const response = await fetch('https://country.register.gov.uk/records.json?page-size=5000');
+      await sleep(1e3); // For demo purposes.
+      const countries = await response.json();
+
+      if (active) {
+        setOptions(Object.keys(countries).map((key) => countries[key].item[0]));
+      }
+    })();
+
+    return () => {
+      active = false;
+    };
+  }, [loading]);
+
+  React.useEffect(() => {
+    if (!open) {
+      setOptions([]);
+    }
+  }, [open]);
 
   return (
     <div className="App">
-        <AppBar className={classes.grow} position="static">
-          <Toolbar>
-            <Typography variant="h6" className={classes.grow} noWrap>
-              Animal Crossing Birthdays
+      <AppBar className={classes.grow} position="static">
+        <Toolbar>
+          <Typography variant="h6" className={classes.grow} noWrap>
+            Animal Crossing Birthdays
             </Typography>
 
-            <IconButton color="inherit" aria-label="search">
-              <SearchIcon />
-            </IconButton>
+          <IconButton color="inherit" aria-label="search">
+            <SearchIcon onClick={handleClick} />
+          </IconButton>
 
-            <IconButton edge="end" color="inherit" aria-label="menu">
-              <MenuIcon />
-            </IconButton>
-          </Toolbar>
-        </AppBar>
+          <Popover
+            id={id}
+            open={o}
+            anchorEl={anchorEl}
+            onClose={handleClose}
+            anchorOrigin={{
+              vertical: 'bottom',
+              horizontal: 'center',
+            }}
+            transformOrigin={{
+              vertical: 'top',
+              horizontal: 'center',
+            }}
+          >
+
+            <Autocomplete
+              id="asynchronous-demo"
+              style={{ width: 300 }}
+              open={open}
+              onOpen={() => {
+                setOpen(true);
+              }}
+              onClose={() => {
+                setOpen(false);
+              }}
+              getOptionSelected={(option, value) => option.name === value.name}
+              getOptionLabel={(option) => option.name}
+              options={options}
+              loading={loading}
+              renderInput={(params) => (
+                <TextField
+                  {...params}
+                  label="Search"
+                  variant="outlined"
+                  InputProps={{
+                    ...params.InputProps,
+                    endAdornment: (
+                      <React.Fragment>
+                        {loading ? <CircularProgress color="inherit" size={20} /> : null}
+                      </React.Fragment>
+                    ),
+                  }}
+                />
+              )}
+            />
+
+          </Popover>
+
+          <IconButton edge="end" color="inherit" aria-label="menu">
+            <MenuIcon onClick={handleClick} />
+          </IconButton>
+
+          <Menu
+            id="simple-menu"
+            anchorEl={anchorEl}
+            keepMounted
+            open={Boolean(anchorEl)}
+            onClose={handleClose}
+          >
+            <MenuItem onClick={handleClose}>Home</MenuItem>
+            <MenuItem onClick={handleClose}>Events</MenuItem>
+            <MenuItem onClick={handleClose}>Birthday</MenuItem>
+          </Menu>
+        </Toolbar>
+      </AppBar>
 
 
     </div>
