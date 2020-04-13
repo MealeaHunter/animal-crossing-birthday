ee3172caa33a71d6069100e8253d3d2656805469
Mealea Hunter <h.mealea@gmail.com>
2020-04-12 20:59:50 -0400

    Stylize card & WIP

--- src/components/todays-birthday.js
+++ src/components/todays-birthday.js
@@ -2,12 +2,12 @@ import React from "react";
 import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
 import Card from "@material-ui/core/Card";
-import CardActionArea from "@material-ui/core/CardActionArea";
 import CardActions from "@material-ui/core/CardActions";
 import CardContent from "@material-ui/core/CardContent";
 import CardMedia from "@material-ui/core/CardMedia";
 import Button from "@material-ui/core/Button";
 import { makeStyles } from "@material-ui/core/styles";
+import "../SCSS/todays-birthday.scss";
 
 const useStyles = makeStyles({
   root: {
@@ -19,13 +19,8 @@ function TodaysBirthday() {
   const classes = useStyles();
 
   return (
-    <div>
-      <Grid
-        container
-        spacing={3}
-        direction="column"
-        alignItems="center"
-      >
+    <div className="todays-birthday">
+      <Grid container spacing={3} direction="column" alignItems="center">
         <Grid item xs={12}>
           <Typography variant="h5" noWrap>
             Happy Birthday
@@ -34,20 +29,18 @@ function TodaysBirthday() {
 
         <Grid item xs={12}>
           <Card className={classes.root}>
-            <CardActionArea>
-              <CardMedia
-                component="img"
-                alt="Animal Crossing Character"
-                height="200"
-                image="https://nookipedia.com/w/images/9/95/Isabelle_NH.png"
-                title="Animal Crossing Character"
-              />
-              <CardContent>
-                <Typography gutterBottom variant="h5" component="h2">
-                  Isabelle
-                </Typography>
-              </CardContent>
-            </CardActionArea>
+            <CardMedia
+              component="img"
+              alt="Animal Crossing Character"
+              height="200"
+              image="https://nookipedia.com/w/images/9/95/Isabelle_NH.png"
+              title="Animal Crossing Character"
+            />
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="h2">
+                Isabelle
+              </Typography>
+            </CardContent>
             <CardActions>
               <Button size="small" color="primary">
                 Learn More
