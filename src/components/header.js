8f3cc39cfda1885dbec9f4a24ab99e4c1fb6fd6d
Mealea Hunter <h.mealea@gmail.com>
2020-04-13 12:33:50 -0400

    Reduce unneeded spacing on birthday card

--- src/components/header.js
+++ src/components/header.js
@@ -1,135 +1,27 @@
-import React, { useState, useEffect } from "react";
-import fetch from "cross-fetch";
+import React, { useState } from "react";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import IconButton from "@material-ui/core/IconButton";
 import MenuIcon from "@material-ui/icons/Menu";
-import SearchIcon from "@material-ui/icons/Search";
 import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/core/styles";
 import Popover from "@material-ui/core/Popover";
-import Autocomplete from "@material-ui/lab/Autocomplete";
-import TextField from "@material-ui/core/TextField";
-import CircularProgress from "@material-ui/core/CircularProgress";
 import ClickAwayListener from "@material-ui/core/ClickAwayListener";
 import Paper from "@material-ui/core/Paper";
 import MenuItem from "@material-ui/core/MenuItem";
 import MenuList from "@material-ui/core/MenuList";
 
 function Header() {
-  const [searchAnchorEl, searchSetAnchorEl] = useState(null);
   const [menuAnchorEl, menuSetAnchorEl] = useState(null);
-  const [open, setOpen] = useState(false);
-  const [options, setOptions] = useState([]);
-  const loading = open && options.length === 0;
-
-  const onSearchClick = (event) => {
-    searchSetAnchorEl(event.currentTarget);
-  };
+  const [setOpen] = useState(false);
 
   const onMenuClick = (event) => {
     menuSetAnchorEl(event.currentTarget);
   };
 
   const handleClose = () => {
-    searchSetAnchorEl(null);
     menuSetAnchorEl(null);
   };
-
-  /*
-    Hardcoded for now
-    */
-  const characters = [
-    { name: "Iabelle" },
-    { name: "Tom Nook" },
-    { name: "Ace" },
-  ];
-
-  //   useEffect(() => {
-  //     let active = true;
-
-  //     if (!loading) {
-  //       return undefined;
-  //     }
-
-  //     (async () => {
-  //       const response = await fetch(
-  //         "https://country.register.gov.uk/records.json?page-size=5000"
-  //       );
-
-  //       await response.json();
-
-  //       if (active) {
-  //         setOptions(
-  //           Object.keys(characters).map((key) => characters[key].item[0])
-  //         );
-  //       }
-  //     })();
-
-  //     return () => {
-  //       active = false;
-  //     };
-  //   }, [loading]);
-
-  //   useEffect(() => {
-  //     if (!open) {
-  //       setOptions([]);
-  //     }
-  //   }, [open]);
-
-  // <IconButton color="inherit" aria-label="search">
-  //           <SearchIcon onClick={onSearchClick} />
-  //         </IconButton>
-
-  //         <Popover
-  //           id={"search-character"}
-  //           open={Boolean(searchAnchorEl)}
-  //           anchorEl={searchAnchorEl}
-  //           onClose={handleClose}
-  //           anchorOrigin={{
-  //             vertical: "bottom",
-  //             horizontal: "center",
-  //           }}
-  //           transformOrigin={{
-  //             vertical: "top",
-  //             horizontal: "right",
-  //           }}
-  //         >
-  //           <Autocomplete
-  //             id="character-autocomplete"
-  //             style={{ width: 300 }}
-  //             open={open}
-  //             onOpen={() => {
-  //               setOpen(true);
-  //             }}
-  //             onClose={() => {
-  //               setOpen(false);
-  //             }}
-  //             getOptionSelected={(characters, value) =>
-  //               characters.name === value.name
-  //             }
-  //             getOptionLabel={(characters) => characters.name}
-  //             options={characters}
-  //             loading={loading}
-  //             renderInput={(params) => (
-  //               <TextField
-  //                 {...params}
-  //                 label="Search Character"
-  //                 variant="outlined"
-  //                 InputProps={{
-  //                   ...params.InputProps,
-  //                   endAdornment: (
-  //                     <React.Fragment>
-  //                       {false ? (
-  //                         <CircularProgress color="inherit" size={20} />
-  //                       ) : null}
-  //                     </React.Fragment>
-  //                   ),
-  //                 }}
-  //               />
-  //             )}
-  //           />
-  //         </Popover>
 
   const useStyles = makeStyles((theme) => ({
     grow: {
@@ -138,6 +30,9 @@ function Header() {
     typography: {
       padding: theme.spacing(2),
     },
+    button: {
+        padding: 0
+    }
   }));
 
   const classes = useStyles();
@@ -156,7 +51,7 @@ function Header() {
           Animal Crossing Birthdays
         </Typography>
 
-        <IconButton edge="end" color="inherit" aria-label="menu">
+        <IconButton edge="end" className={classes.button} color="inherit" aria-label="menu">
           <MenuIcon onClick={onMenuClick} />
         </IconButton>
 
--- src/components/todays-birthday.js
+++ src/components/todays-birthday.js
@@ -3,7 +3,6 @@ import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
 import Card from "@material-ui/core/Card";
 import CardActions from "@material-ui/core/CardActions";
-import CardContent from "@material-ui/core/CardContent";
 import CardMedia from "@material-ui/core/CardMedia";
 import Button from "@material-ui/core/Button";
 import { makeStyles } from "@material-ui/core/styles";
@@ -20,7 +19,7 @@ function TodaysBirthday() {
 
   return (
     <div className="todays-birthday">
-      <Grid container spacing={3} direction="column" alignItems="center">
+      <Grid container spacing={2} direction="column" alignItems="center">
         <Grid item xs={12}>
           <Typography variant="h5" noWrap>
             Happy Birthday
@@ -36,15 +35,13 @@ function TodaysBirthday() {
               image="https://nookipedia.com/w/images/9/95/Isabelle_NH.png"
               title="Animal Crossing Character"
             />
-            <CardContent>
-              <Typography gutterBottom variant="h5" component="h2">
-                Isabelle
-              </Typography>
-            </CardContent>
-            <CardActions>
-              <Button size="small" color="primary">
-                Learn More
-              </Button>
+            <CardActions disableSpacing>
+                <Typography variant="h5" component="h2">
+                    Isabelle
+                </Typography>
+                <Button size="small" color="primary">
+                    Learn More
+                </Button>
             </CardActions>
           </Card>
         </Grid>
